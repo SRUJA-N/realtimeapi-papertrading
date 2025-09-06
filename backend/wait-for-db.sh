cfeb7487c3bc07a8b30ab2750488c040d92e6e9e
SRUAJN R <codewithsrujan.r@gmail.com>
2025-09-06 23:08:06 +0530

    Create wait-for-db.sh
    added the final error

---
+++ backend/wait-for-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Wait for PostgreSQL to be ready
+
+set -e
+
+host="$1"
+port="$2"
+shift 2
+cmd="$@"
+
+until nc -z "$host" "$port"; do
+  echo "Waiting for PostgreSQL at $host:$port..."
+  sleep 1
+done
+
+echo "PostgreSQL is up - executing command"
+exec $cmd
